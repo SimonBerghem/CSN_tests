59bc2db97b8ec2d09f058d29a54da347d6711ee6
Simon <bersim-8@student.ltu.se>
2021-09-28 09:30:51 +0200

    init

---
+++ src/src/ltu/PaymentTest.java
@@ -0,0 +1,29 @@
+package src.ltu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PaymentTest
+{
+    @Test
+    public void testSilly()
+    {
+        assertEquals(1, 1);
+    }
+
+    // Age requirements
+
+
+    // Study pace requirements
+
+    // Income while studying requirements
+
+    // Completion ratio requirement
+
+    // Full time students compensation
+
+    // Halftime students compensation
+
+    // Receiving compensation
+}
