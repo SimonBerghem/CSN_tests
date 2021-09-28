3978757de5b8cd4b394476f304daa7ff9397eb7d
Erik Serrander <s7rul@ludd.ltu.se>
2021-09-28 10:36:00 +0200

    setup

--- src/src/ltu/PaymentTest.java
+++ src/src/ltu/PaymentTest.java
@@ -5,11 +5,26 @@ import static src.ltu.CalendarFactory.getCalendar;
 
 
 import org.junit.Test;
+import org.junit.Before;
 
 import java.io.IOException;
 
 public class PaymentTest
 {
+    private PaymentImpl testPaymentImpl;
+
+    @Before
+    public void setup() {
+        String DEFAULT_RULES = "student100loan=7088\nstudent100subsidy=2816\nstudent50loan=3564\nstudent50subsidy=1396\nstudent0loan=0\nstudent0subsidy=0\nfulltimeIncome=85813\nparttimeIncome=128722\n";
+        String className = System.getProperty("calendar", "src.ltu.CalenderForTesting");
+        ICalendar cal = getCalendar(className);
+        try {
+            testPaymentImpl = new PaymentImpl(cal, DEFAULT_RULES);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
     public void testSilly()
     {
@@ -19,12 +34,8 @@ public class PaymentTest
     // Age requirements
     @Test
     public void id102AgeOver20() throws IOException {
-        String DEFAULT_RULES = "student100loan=7088\nstudent100subsidy=2816\nstudent50loan=3564\nstudent50subsidy=1396\nstudent0loan=0\nstudent0subsidy=0\nfulltimeIncome=85813\nparttimeIncome=128722\n";
-        String className = System.getProperty("calendar", "src.ltu.CalenderForTesting");
-        ICalendar cal = getCalendar(className);
-        PaymentImpl t = new PaymentImpl(cal, DEFAULT_RULES);
 
-        int amount = t.getMonthlyAmount("19980121-2867", 0, 100, 100);
+        int amount = testPaymentImpl.getMonthlyAmount("19980121-2867", 0, 100, 100);
 
         assertEquals(7088+2816, amount);
     }
