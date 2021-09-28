8f1f40a8cccb6cbc7e2b4bf4fbdb54e1b8d35d31
Erik Serrander <s7rul@ludd.ltu.se>
2021-09-28 10:23:18 +0200

    fix

--- src/src/ltu/CalenderForTesting.java
+++ src/src/ltu/CalenderForTesting.java
@@ -7,7 +7,11 @@ import java.util.Date;
 
 public class CalenderForTesting implements ICalendar {
     @Override
-    public Date getDate() throws ParseException {
-        return new SimpleDateFormat("yyyy-MM-dd").parse("2021-09-28");
+    public Date getDate() {
+        try {
+            return new SimpleDateFormat("yyyy-MM-dd").parse("2021-09-28");
+        } catch (ParseException e) {
+            return null;
+        }
     }
 }
