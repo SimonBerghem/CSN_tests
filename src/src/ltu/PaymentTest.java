f58f7cf0b9ce22e317e336ac9127226e79113c18
Erik Serrander <s7rul@ludd.ltu.se>
2021-09-28 10:29:53 +0200

    fixat

--- src/src/ltu/PaymentTest.java
+++ src/src/ltu/PaymentTest.java
@@ -20,11 +20,11 @@ public class PaymentTest
     @Test
     public void id102AgeOver20() throws IOException {
         String DEFAULT_RULES = "student100loan=7088\nstudent100subsidy=2816\nstudent50loan=3564\nstudent50subsidy=1396\nstudent0loan=0\nstudent0subsidy=0\nfulltimeIncome=85813\nparttimeIncome=128722\n";
-        String className = System.getProperty("calendar", "ltu.CalenderForTesting");
+        String className = System.getProperty("calendar", "src.ltu.CalenderForTesting");
         ICalendar cal = getCalendar(className);
         PaymentImpl t = new PaymentImpl(cal, DEFAULT_RULES);
 
-        int amount = t.getMonthlyAmount("100121-2867", 0, 100, 100);
+        int amount = t.getMonthlyAmount("19980121-2867", 0, 100, 100);
 
         assertEquals(7088+2816, amount);
     }
